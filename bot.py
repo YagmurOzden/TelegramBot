786e928d99becfc8e10e23e2d23e81db8ad48a16
YagmurOzden <57031699+YagmurOzden@users.noreply.github.com>
2020-02-13 01:10:22 +0300

    Acquiring Data
    Taking data from a web sites

--- bot.py
+++ bot.py
@@ -10,16 +10,14 @@ r = requests.get(url)
 soup=BeautifulSoup(r.content,"html.parser")
 
 gelen_veri =soup.find_all("ul", {"class": "home-table"})
-i=0
 for y in gelen_veri:
     yemek_bas=y.find_all("li",{"data-toggle":"tooltip"})
     src=y.find_all("li")
-    if i<20:
-        src=src[i].text
-        src = src.replace("\r", "")
-        src = src.replace("\n", "")
-        i+=6
-        print (src)
+
+    src=src[0].text
+    src = src.replace("\r", "")
+    src = src.replace("\n", "")
+    print (src)
 
     tarih=src[0:30]
 
